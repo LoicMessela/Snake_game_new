4ac506cbf310480035fd25d91bc1b4a987c9fdc1
Loïc Messela <messelaloic@gmail.com>
2023-02-02 17:14:18 +0100

    Game fully fonctional

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Snake_game_new
+# SNAKE GAME
+
+I've build a simple snake game. A snake is displayed on the screen and grows each time he eat the fruit that is displayed. The score updates by 10 each time the snake eats. The user can move accross the walls. Bitting the tail causes the snakes dead and ends the game.
+
+## key Features
+
+- Snakes speed : 200ms;
+- Snake crosses the wall and can grow very long;
+- Food appears randomly on the screen
+
+## Tech Stack
+
+- HTML
+- CSS
+- JavaScript
+
+## What's next?
+
+- Reduce the length of my code (mainly JS);
+- Add limited number of lives to the snake;
+- Display a game menu;
+- Add some new levels to increase the difficulty;
+- Save the highest scores of the players.
+
+1. Clone the repo
+
+```bash
+git clone https://your-repo-url.git
+```
+
+2. Install dependencies
+
+```bash
+npm install
+```
+
+3. Start the dev server
+
+```bash
+npm run dev
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is open source and available under the [MIT License](LICENSE).
--- src/canvas.js
+++ src/canvas.js
@@ -2,11 +2,16 @@ const gridElement = document.getElementById("grid");
 const startButton = document.getElementById("start-game");
 const scoreElement = document.getElementById("score");
 
+const pauseButton = document.getElementById("pause");
+const modal = document.getElementById("dialog");
+const resetButton = modal.querySelector("#reset");
+
 const columns = 10;
 const rows = 10;
 let cells = [];
 let score = 0;
 let currentPosition = 0;
+let interval = null;
 
 const snake = {
   positions: [(columns * rows) / 2],
@@ -46,11 +51,9 @@ const snake = {
         }
         break;
     }
-    /*console.log(this.positions, nextPosition, currentPosition);*/
-    if (this.positions.includes(nextPosition)) {
-      alert("You Lose");
 
-      function endGame() {}
+    if (this.positions.includes(nextPosition)) {
+      return endGame();
     }
 
     //console.log(snakeHead)
@@ -64,22 +67,28 @@ const snake = {
     this.direction = newDirection;
   },
   snakeEats() {
-    //console.log(`snakeposition:${snake.positions}`)
-    //console.log(`currentPosition: ${currentPosition}`)
-
     if (cells[currentPosition].classList.contains("food")) {
       createFood(1);
       snake.positions.unshift(currentPosition);
       score += 10;
       this.displayScore();
-      console.log(score);
     }
   },
   displayScore() {
     scoreElement.textContent = score;
-    //newScore.textContent = score
   },
 };
+
+function endGame() {
+  clearInterval(interval);
+  modal.showModal();
+
+  // alert();
+
+  // show game over popup
+  // popup should have a restart button
+  // when restart is clicked, you reset the score and snake position. and then start game
+}
 
 function createTheGrid() {
   for (let i = 0; i < columns * rows; i++) {
@@ -120,22 +129,26 @@ function startTheGame() {
   score = 0;
   gridElement.innerHTML = "";
   cells = [];
+  snake.positions = [(columns * rows) / 2];
   currentPosition = 0;
   createTheGrid();
   displaySnake();
-  displayScore();
-  snake.positions;
+  displayScore(0);
+  snake.direction = "right";
   createFood(1);
+  startButton.disabled = true;
 }
 //let theInterval;
 function refresh() {
-  setInterval(() => {
+  interval = setInterval(() => {
     displaySnake("hide");
-    // snake.move();
+    snake.move();
     snake.snakeEats();
     displaySnake();
   }, 200);
 }
+
+//
 
 window.addEventListener("keydown", (event) => {
   //console.log(event.code);
@@ -162,12 +175,22 @@ window.addEventListener("keydown", (event) => {
   }
 });
 
-startTheGame();
-refresh();
+// startTheGame();
+
+resetButton.addEventListener("click", () => {
+  scoreElement.textContent = '0';
+  modal.close();
+  startTheGame();
+  refresh();
+});
 
 startButton.addEventListener("click", () => {
   snake.positions;
+  refresh();
   startTheGame();
+
 });
+
+
 
 function displayScore() {}
